6a26f37239def7abf4689247e2d834f4c2968cc2
Daniele Galiffa <daniele.mentegrafica@gmail.com>
2020-03-27 18:49:24 +0100

    -- added target blank

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-var mapLink = '&copy; <a href="//openstreetmap.org">OpenStreetMap</a> Contributors';
-var fontLink = '<a href="//fontawesome.io">Font Awesome by Dave Gandy - http://fontawesome.io</a>';
-var authorLink = 'map created by <a href="//it.linkedin.com/in/danielegaliffa">Daniele Galiffa</a>';
-var dataLink = 'source data: <a href="//www.comune.brindisi.it/brindisi/images/servizi_aggiuntivi/coronavirus/elenco_attivita_consegna_a_domicilio_27marzo.pdf">Comune di Brindisi</a> last updated on 2020.03.27';
+var mapLink = '&copy; <a target="_blank" href="//openstreetmap.org">OpenStreetMap</a> Contributors';
+var fontLink = '<a target="_blank" href="//fontawesome.io">Font Awesome by Dave Gandy - http://fontawesome.io</a>';
+var authorLink = 'map created by <a target="_blank"href="//it.linkedin.com/in/danielegaliffa">Daniele Galiffa</a>';
+var dataLink = 'source data: <a target="_blank" href="//www.comune.brindisi.it/brindisi/images/servizi_aggiuntivi/coronavirus/elenco_attivita_consegna_a_domicilio_27marzo.pdf">Comune di Brindisi</a> last updated on 2020.03.27';
 
 var no_cache = Math.random().toString().replace(".","");
 var data_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTB7QuheI8mzSxqLWEHU0eRA7QlmICN3p18czaVEfFI_r9WNtRfQMK4cmWJr5yyIFi8hDHfaRLCXo9Z/pub?gid=0&single=true&output=tsv&p=' + no_cache;
